a669f68b81954ead906902f5e228ad4634b6cd44
liuwx <971465407@qq.com>
2020-12-10 20:12:01 +0800

    create Class types

---
+++ src/main/java/com/example/demo/reflection/CreateClass.java
@@ -0,0 +1,35 @@
+package com.example.demo.reflection;
+
+public class CreateClass {
+}
+
+class Person{
+    private String name;
+
+    public Person() {
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
+
+class Student extends Person{
+
+
+}
